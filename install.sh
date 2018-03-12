7fce2dd19c214a7e630b37127d105d6c331b1324
Giacomo Triggiano <giacomotriggiano@gmail.com>
2018-03-12 17:12:06 +0100

    ...

--- install.sh
+++ install.sh
@@ -7,9 +7,8 @@ set -e
 apt-get update
 
 # install s3 tools
-apt-get install -y python python-pip
+apt-get install -y python python-pip curl
 pip install awscli
-apt-get purge -y python-pip
 
 # install go-cron
 curl -L --insecure https://github.com/odise/go-cron/releases/download/v0.0.7/go-cron-linux.gz | zcat > /usr/local/bin/go-cron
